c697d88d149311255f39e2e2e8bf1587a41e0df1
dugol <danielugiraldo@gmail.com>
2018-05-19 11:26:02 -0500

    prueba terminada

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var cors = require('cors')
 var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
-var codebreaker = require ('./functionTest/codeBreaker1');
+var secret =[0,0,0,0];
+var codebreaker = require('./functionTest/codeBreaker1');
 
 const port = process.env.PORT || 8000;
 
@@ -12,14 +13,36 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+setSecret();
+
+function ramdomSecret(){
+    var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var secret=[];
+
+    for (var i = 1; i <= 4; i++) {
+        var num = Math.floor(Math.random() * array.length);
+        var roll = array.splice(num, 1);
+        var yourNumber = roll[0];
+        secret.push(yourNumber);
+    }
+    console.log(secret);
+    return secret;
+    
+}
+
+function setSecret() {
+    secret = ramdomSecret();
+}
 
 // ingresar palabra a la bd
-app.get('/api/codebreaker/:number',function(req, res){	
+app.get('/api/codebreaker/:number', function (req, res) {
     var number = req.params.number;
-    codebreaker.setSecret([1,2,3,4]);
-	res.send(JSON.stringify(codebreaker.game(number)));
+    
+
+    codebreaker.setSecret(secret);
+    res.send(JSON.stringify(codebreaker.game(number)));
 });
 
-server.listen(port, function(){
-	console.log("Corriendo por el puerto "+port)
+server.listen(port, function () {
+    console.log("Corriendo por el puerto " + port)
 });
